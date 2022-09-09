59e43c097f0a4d614bbca1f14d1fbd8bb5a2ca07
Yunuzz <yoonusonoruoiza@gmail.com>
2022-09-09 05:38:30 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main-Printing
+ *
+ *Return: Always 0 (Successful)
+ */
+int main (void)
+{
+	put("\"Programming is like building a multilingual puzzle");
+return(0);
+}
