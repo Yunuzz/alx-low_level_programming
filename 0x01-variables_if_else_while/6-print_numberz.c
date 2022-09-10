367e002cd1506e2992064bdcdc665b44ed1fe40a
Yunuzz <yoonusonoruoiza@gmail.com>
2022-09-09 20:30:14 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints number 0 to 9
+ *
+ * Return:Always (success)
+ */
+
+int main(void)
+{
+	int n:
+
+		for (n = '0'; n <= '9'; n++)
+		{
+			putchar(n)
+		}
+	putchat('\n')
+
+		return (0);
+}
+
