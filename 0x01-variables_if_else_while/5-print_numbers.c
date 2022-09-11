0542dc5c9519b3388f7c687ea9bcecb95cc4c97c
Yunuzz <yoonusonoruoiza@gmail.com>
2022-09-11 15:45:57 -0700

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,20 +1,59 @@
 #include <stdio.h>
 
 /**
+ *
  *  * main - Entry point
- *   *
- * Return: Always 0 (Success)
+ *
+ * Description: main - prints all possible combinations 2
+ *
+ * Return: 0
+ *
  */
+
 int main(void)
+
 {
-		int d = 48;
 
-			while (d < 58)
-					{
-								putchar(d);
-										d++;
-											}
-				putchar('\n');
+	int i, j;
 
-					return (0);
+	for (i = 0; i < 100; i++)
+
+	{
+
+	for (j = 0; j < 100; j++)
+
+	{
+
+	if (i < j)
+
+	{
+
+	putchar((i / 10) + 48);
+
+	putchar((i % 10) + 48);
+
+	putchar(' ');
+
+	putchar((j / 10) + 48);
+
+	putchar((j % 10) + 48);
+
+	if (i != 98 || j != 99)
+
+	{
+
+	putchar(',');
+
+	putchar(' ');
+
+	}
+	
+	}
+
+	}
+	}
+	putchar('\n');
+
+	return (0);
+
 }
