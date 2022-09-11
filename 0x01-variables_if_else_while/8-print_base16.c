44ff6da487c484738e7457e1ad455102a7c60c14
Yunuzz <yoonusonoruoiza@gmail.com>
2022-09-10 21:24:21 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints a hexadecial string
+ *
+ * Retuen:Always(success)
+ */
+int ain(void)
+{
+	char c;
+
+	for (c = '0'; c = '9'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+	return (0);
+}
