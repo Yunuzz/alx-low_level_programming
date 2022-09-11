60403b446c6f83569503b9a837c36f922fdba643
Yunuzz <yoonusonoruoiza@gmail.com>
2022-09-11 16:32:49 -0700

    persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 /**
- * main - prints a series of numbers with commas
- * return - always 0(success)
+ * main - Entry point
+ *
+ * Description: using the main function
+ * this program prints "0, 1, 2, 3, 4, 5, 6, 7, 8, 9"
+ * Return: 0
  */
+
 int main(void)
 {
-int c;
-	for (c = '0'; c <= '9'; c++);
-{
-putchar(c);
-	if (c != '9')
+	int c;
 
-{
-putchar(',');
-putchar(' ');
-} 
-}
+	for (c = 48; c <= 57; c++)
+	{			
+	putchar(c);
+	if (c != 57)
+	{
+	putchar(',');	
+	putchar(' ');
+	}
+	}
 	putchar('\n');
 	return (0);
 }
