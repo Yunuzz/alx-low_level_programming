a1a0b77bd11d1a9e1f3ce988ffef15cf5647a804
Yunuzz <yoonusonoruoiza@gmail.com>
2022-09-11 16:10:39 -0700

    Smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,36 +1,21 @@
 #include <stdio.h>
+
 /**
- *
  * main - Entry point
  *
  * Description: using the main function
- *
  * this program prints "Programming is positive, zero or negative
- *
  * Return: 0
- *
  */
 
 int main(void)
-
 {
-
 	char c;
 
-
-
 	for (c = 'z'; c >= 'a'; c--)
-
 	{
-		{
-			continue;
-		}
 	putchar(c);
-
 	}
-
 	putchar('\n');
-
 	return (0);
-
 }
