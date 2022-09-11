7e64f879024a661ec29fd7cccbe95954f3aca0d4
Yunuzz <yoonusonoruoiza@gmail.com>
2022-09-10 23:13:37 -0700

    Inventing is a combination of brains and materials.

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,45 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *  * main - main block
- *   * Description: Write a program that prints all possible
- *    * different combinations of two digits
- *     * Numbers must be separated by ,, followed by a space
- *      * The two digits must be different
- *       * 01 and 10 are considered the same combination of the two digits 0 and 1
- *        * Print only the smallest combination of two digits
- *         * Numbers should be printed in ascending order, with two digits
- *          * You can only use the putchar function
- *           * (every other function (printf, puts, etc…) is forbidden)
- *            * You can only use putchar five times maximum in your code
- *             * You are not allowed to use any variable of type char
- *              * All your code should be in the main function
- *               * Return: 0
- *                */
+ * main - prints single digit numbers starting from 0
+ *
+ * return: Returns 0 at the end
+ */
 int main(void)
 {
-		int c;
-			int d = 0;
+	int i, j;
+	for (i = '0'; i <= '9'; i++)
 
-				while (d < 10)
-						{
-									c = 0;
-											while (c < 10)
-														{
-																		if (d != c && d < c)
-																						{
-																											putchar('0' + d);
-																															putchar('0' + c);
-
-																																			if (c + d != 17)
-																																								{
-																																														putchar(',');
-																																																			putchar(' ');
-																																																							}
-																																						}
-
-																					c++;												}d++;
+	{
+		for (j = '1'; j <= '9'; j++)
+	{
+		if (j > i)
+		{
+			putchar(i + '0');
+				putchar(j + '0');
+				if (i != '8')
+				{
+					putchar(',');
+						putchar(' ');
+				}
+		}
+	}
+	}
+putchar('\n')
+	return (0);
 }
-putchar('\n');
-return (0);
-}
+
