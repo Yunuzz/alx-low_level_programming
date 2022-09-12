808276ec5eda515845619af087ff6e4d315eb980
Yunuzz <yoonusonoruoiza@gmail.com>
2022-09-11 19:53:01 -0700

    The success combination in business is

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,47 +1,38 @@
 #include <stdio.h>
-
 /**
- * main - program that prints all possible different combinations of
- * three digits
- * Numbers must be separated by ,followed by a space
- * The two digits must be different
- * 012, 120, 102, 021, 201, 210 are considered the same combination
+ * main - Entry point
+ *
+ * Description: using the main function
+ * this "prints all possible different combinations of 3 digits
  * Return: 0
  */
 
 int main(void)
 {
-		int n1 = 48;
-		int n2, n3;
-		int com = 44;
+	int c = 0;
+	int b;
+	int d;
+	int f;
 
-		while (n1 <= 57)
-{
-	n2 = n1 + 1;
-	while (n2 <= 57)
-{
-																			n3 = n2 + 1;
-																			while (n3 <= 57)
-																
-{	
-	putchar(n1);
-																																			putchar(n2);
-																																			putchar(n3);
-																																		if (n1 != 55 || n2 != 56 || n3 != 57)
-																																									{
-																																																			putchar(com);
-																																																			putchar(32);
-		
+	while (c <= 999)
+	{
+	b = (c / 100 + '0');
+	d = (c / 10 % 10 + '0');
+	f = (c % 10 + '0');
+	if ((b < d) && (d < f))
+	{
+	putchar(b);
+	putchar(d);
+	putchar(f);
 
-																																		}
-
-																																		n3 += 1;
-						
-}
-																	n2 += 1;
-				
-																	}
-		n1 += 1;
-}
-		putchar('\n');
+	if (c != 789)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	c++;
+	}
+	putchar('\n');
 	return (0);
+}
