4359177c423c8415eda5988954fd5fcadb92b008
Yunuzz <yoonusonoruoiza@gmail.com>
2022-09-13 19:32:06 -0700

    nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,17 +20,15 @@ void print_times_table(int n)
 
 	if (j == 0)
 	{
-	_putchar('0');
-	}
-	else if (prod <= 9)
+		_putchar('0');
+	}else if (prod <= 9)
 	{
 	_putchar(',');
 	_putchar(' ');
 	_putchar(' ');
 	_putchar(' ');
 	_putchar('0' + prod);
-	}
-	else if (prod > 99)
+	}else if (prod > 99)
 	{
 	_putchar(',');
 	_putchar(' ');
